60fa3647998cc6dd345afc201e1fe5f18cd1448c
Harvey Hu <harveyhupersonal@gmail.com>
2026-07-24 17:15:25 -0700

    rope optimized toy example

--- cs336_basics/rope.py
+++ cs336_basics/rope.py
@@ -8,8 +8,22 @@ class RoPE(nn.Module):
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device: torch.device = None):
         super().__init__()
         assert d_k % 2 == 0
-
         
+        i_mat = rearrange(
+            torch.arange(max_seq_len, device=device),
+            "max_seq_len -> max_seq_len 1"
+        )
+        k_mat = rearrange(
+            torch.arange(1, d_k // 2+1, device=device),
+            "half -> 1 half"
+        )
+        
+        theta_mat = i_mat / (theta ** ((2 * k_mat - 2) / d_k)) # broadcast -> (max_seq_len, half)
+        cos_mat = torch.cos(theta_mat)
+        sin_mat = torch.sin(theta_mat) # (max_seq_len, half)
+
+        self.register_buffer("cos_mat", cos_mat, persistent=False)
+        self.register_buffer("sin_mat", sin_mat, persistent=False)
 
     def forward(
         self, 
@@ -24,49 +38,37 @@ if __name__ == "__main__":
     test_tensor = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]], dtype=torch.float32)
     # seq_len, d_k
     print(test_tensor)
-    rearranged = rearrange(test_tensor, "... (half two) -> ... half two", two=2)
+    rearranged = rearrange(test_tensor, "... seq_len (half two) -> ... seq_len half two", two=2)
     print(rearranged, rearranged.shape)
-
     # rearranged = rearrange(test_tensor, "... (two half) -> ... half two", two=2)
     # print(rearranged, rearranged.shape)
 
     # rearranged = rearrange(test_tensor, "... (half two) -> ... two half", two=2)
     # print(rearranged, rearranged.shape)
 
-    cos = 1
-    sin = 1
-
-    def mat_ik(i, k):
-        return torch.tensor(
-            [
-                [cos*i/k, -sin*i/k],
-                [sin*i/k, cos*i/k]
-            ],
-            dtype=torch.float32
-        )
-
-    def mat_i(i):
-        return torch.stack(
-            [mat_ik(i, k) for k in range(1, d_k // 2+1)]
-        )
-    print("mat_1", mat_i(1), mat_i(1).shape)
-
     max_seq_len = 10
-    mat_full = torch.stack(
-        [mat_i(i) for i in range(max_seq_len)]
+
+    i_mat = rearrange(
+        torch.arange(max_seq_len),
+        "max_seq_len -> max_seq_len 1"
     )
-    print(mat_full.shape) # max_seq_len, d_k // 2, 2, 2
+    k_mat = rearrange(
+        torch.arange(1, d_k // 2+1),
+        "half -> 1 half"
+    )
+    
+    th = 1000
+    theta_mat = i_mat / (th ** ((2 * k_mat - 2) / d_k)) # broadcast -> (max_seq_len, half)
+    cos_mat = torch.cos(theta_mat)
+    sin_mat = torch.sin(theta_mat) # (max_seq_len, half)
 
     token_positions = torch.tensor([1]) # ... seq_len
 
-    relevant_mat = mat_full[token_positions] # seq_len, dk // 2, 2, 2
-
-    res = einsum(
-        rearranged,
-        relevant_mat,
-        "... half two, ... half other_two two -> ... half other_two"
-    )
-    print(res, res.shape)
-
-    res = rearrange(res, "... half two -> ... (half two)")
+    left, right = rearranged[..., 0], rearranged[..., 1]  # ... seq_len half
+    print("left, right", left.shape, right.shape)
+    cos_rel = cos_mat[token_positions] # ... seq_len, half
+    sin_rel = sin_mat[token_positions] # ... seq_len, half
+    top = cos_rel * left - sin_rel * right
+    bottom = sin_rel * left + cos_rel * right # ... seq_len, half
+    res = rearrange(torch.stack([top, bottom], dim=-1), "... seq_len half two -> ... seq_len (half two)")
     print(res, res.shape)

a274b0d401a4beeec69ef8e3ef3f02e8f8f63581
Harvey Hu <harveyhupersonal@gmail.com>
2026-07-24 18:10:59 -0700

    nit

--- cs336_basics/scaled_dot_product_attention.py
+++ cs336_basics/scaled_dot_product_attention.py
@@ -3,6 +3,8 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 import torch
 
+from cs336_basics.softmax import softmax
+
 def scaled_dot_product_attention(
     Q: Float[Tensor, " ... queries d_k"],
     K: Float[Tensor, " ... keys d_k"],
@@ -24,9 +26,7 @@ def scaled_dot_product_attention(
     d_k = Q.shape[-1]
     scores : Float[Tensor, " ... queries keys"] = einsum(Q, K, "... queries d_k, ... keys d_k -> ... queries keys") / d_k**0.5
     if mask is not None:
-        mask = ~mask
-        scores = scores.masked_fill(mask, float("-inf"))
-    from cs336_basics.softmax import softmax
+        scores = scores.masked_fill(~mask, float("-inf"))
     weights = softmax(scores, dim=-1)
     out = einsum(weights, V, "... queries keys, ... keys d_v -> ... queries d_v")
     return out
